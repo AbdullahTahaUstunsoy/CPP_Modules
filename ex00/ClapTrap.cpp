aa9b9fe09aa9fdb799f7387e237b5d85e5e34949
AbdullahTahaUstunsoy <abdullahustunsoy1@gmail.com>
2026-02-02 16:02:00 +0300

    ex00 continue

--- ex00/ClapTrap.cpp
+++ ex00/ClapTrap.cpp
@@ -1,6 +1,10 @@
 #include "ClapTrap.hpp"
 #include <iostream>
 
+ClapTrap::ClapTrap()
+{
+}
+
 ClapTrap::ClapTrap(std::string name) : _name(name)
 {
     std::cout << "Default constructor called." << std::endl;
@@ -25,8 +29,31 @@ ClapTrap::~ClapTrap()
     std::cout << "Destructor called" << std::endl;
 }
 
-void ClapTrap::attack(const std::string& target)
+void ClapTrap::attack(const std::string& target) //Attacking and repairing each cost 1 energy point.
 {
-    std::cout << "ClapTrap " + _name + "attacks" + target + ", causing" + 1 +  "points of damage"<< std::endl;
+    if(_energy_points != 0 && _hit_points != 0)
+    {
+        std::cout << "ClapTrap " << _name << "attacks" << target << ", causing" << 1 << "points of damage"  << std::endl;
+        _energy_points--;
+    }
+    else if (_energy_points == 0 && _hit_points != 0)
+        std::cout << "ClapTrap " << _name << "has no energy" << std::endl;
+    else if (_hit_points == 0 && _energy_points != 0)
+        std::cout << "ClapTrap " << _name << "has no hit points" << std::endl;
+    else    
+        std::cout << "ClapTrap " << _name << "has no energy points and hit points" << std::endl;
 }
 
+void ClapTrap::takeDamage(unsigned int amount)
+{
+
+}
+
+void ClapTrap::beRepaired(unsigned int amount) //When ClapTrap repairs itself, it regains <amount> hit points.
+{
+    std::cout << "ClapTrap " << _name << " is repaired" << amount << " point." << std::endl;
+    if(_hit_points >= 0 && _hit_points < 10)
+        _hit_points++;
+    else
+        std::cout << "hit point full." << std::endl;
+}
--- ex00/ClapTrap.hpp
+++ ex00/ClapTrap.hpp
@@ -7,10 +7,11 @@ class ClapTrap
 {
     private:
         std::string _name;
-        int energy_points = 10; //bunlara atama olmayacağı için "_" ile başlatmadım
-        int hit_points = 10;
-        int attack_damage = 0;
+        int _energy_points = 10; //bunlara atama olmayacağı için "_" ile başlatmadım
+        int _hit_points = 10;
+        int _attack_damage = 0;
     public:
+        ClapTrap();
         ClapTrap(std::string name);
         ClapTrap(const ClapTrap& ref); //const koymama gerek var mı zaten değiştirilemiyor
         ClapTrap& operator=(const ClapTrap& ref);
