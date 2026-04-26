2085430676dedab5ffb0adabd834c61404cc7da6
Abdullah Taha Ustunsoy <abdullahustunsoy1@gmail.com>
2026-04-26 12:41:58 +0300

    casting yapıldı main ekleyeceğim

--- ex00/ScalarConverter.cpp
+++ ex00/ScalarConverter.cpp
@@ -2,6 +2,8 @@
 #include <iostream>
 #include <limits>
 #include <cctype>
+#include <iomanip>
+#include <cmath>
 
 ScalarConverter::ScalarConverter() {}
 ScalarConverter::ScalarConverter(const ScalarConverter& other) { (void)other; }
@@ -129,4 +131,147 @@ int ScalarConverter::detectType(const std::string& literal)
 void ScalarConverter::convert(const std::string& literal)
 {
     int type = detectType(literal);
+
+    if(type == INVALID) //herhangi bir dönüşüm yapılamıyorsa mesela 42.42.42.42 gibi
+    {
+        std::cout << "char: impossible" << std::endl;
+        std::cout << "int: impossible" << std::endl;
+        std::cout << "float: impossible" << std::endl;
+        std::cout << "double: impossible" << std::endl;
+        return;
+    }
+    if(type == PSEUDO)
+    {
+        std::cout << "char: impossible" << std::endl;
+        std::cout << "int: impossible" << std::endl;
+        if(literal[literal.length() - 1] == 'f')
+        {
+            std::cout << "float: " << literal << std::endl;
+            std::cout << "double: " << literal.substr(0, literal.length() - 1) << std::endl;
+        }
+        else
+        {
+            std::cout << "float: " << literal << "f" << std::endl;
+            std::cout << "double: " << literal << std::endl;
+        }
+        return;
+    }
+
+    if(type == CHAR)
+    {
+        if(std::isprint(literal[0]))
+            std::cout << "char: '" << literal[0] << "'" << std::endl;
+        else
+            std::cout << "char: Non displayable" << std::endl;
+        
+        char c = literal[0];
+
+        int i = static_cast<int>(c);
+        float  f = static_cast<float>(c);
+        double d = static_cast<double>(c);
+        std::cout << std::fixed << std::setprecision(1);
+        std::cout << "int: " << i << std::endl;
+        std::cout << "float: " << f << "f" << std::endl;
+        std::cout << "double: " << d << std::endl;
+    }
+
+    if(type == INT)
+    {
+        // String'i önce long'a çeviriyoruz ki int sınırlarını kontrol edebilelim
+        long temp = std::strtol(literal.c_str(), NULL, 10);
+        int i;
+        float f;
+        double d;
+        char c;
+        // Taşma kontrolü (Overflow)
+        bool isIntImpossible = (temp > std::numeric_limits<int>::max() || temp < std::numeric_limits<int>::min());
+        if(!isIntImpossible)
+            i = static_cast<int>(temp);
+        if(isIntImpossible || temp < 0 || temp > 127)
+            std::cout << "char: impossible" << std::endl;
+        else
+        {
+            c = static_cast<char>(i);
+            if(std::isprint(c))
+                std::cout << "char: '" << c << "'" << std::endl;
+            else
+                std::cout << "char: Non displayable" << std::endl;
+        }
+        if(isIntImpossible)
+            std::cout << "int: impossible" << std::endl;
+        else
+            std::cout << "int: " << i << std::endl;
+        if(isIntImpossible)
+        {
+            f = static_cast<float>(temp);
+            d = static_cast<double>(temp);
+        }
+        else
+        {
+            f = static_cast<float>(i);
+            d = static_cast<double>(i);
+        }
+        std::cout << std::fixed << std::setprecision(1);
+        std::cout << "float: " << f << "f" << std::endl;
+        std::cout << "double: " << d << std::endl;
+    }
+
+    if(type == FLOAT)
+    {
+        float f = std::strtof(literal.c_str(), NULL);
+
+        char    c = static_cast<char>(f);
+        int     i = static_cast<int>(f);
+        double  d = static_cast<double>(f);
+
+        //CHAR kontrolü
+        if (f < 0 || f > 127 || std::isnan(f) || std::isinf(f))
+            std::cout << "char: impossible" << std::endl;
+        else
+        {
+            if(std::isprint(c))
+                std::cout << "char: '" << c << "'" << std::endl;
+            else
+                std::cout << "char: Non displayable" << std::endl;
+        }
+        //INT Kontrolü
+        if (f > std::numeric_limits<int>::max() || f < std::numeric_limits<int>::min() || std::isnan(f) || std::isinf(f))
+            std::cout << "int: impossible" << std::endl;
+        else
+            std::cout << "int: " << i << std::endl;
+        
+        std::cout << std::fixed << std::setprecision(1);
+        std::cout << "float: " << f << "f" << std::endl;
+        std::cout << "double: " << d << std::endl;
+    }
+
+    if(type == DOUBLE)
+    {
+        double d = std::strtod(literal.c_str(), NULL);
+
+        char    c = static_cast<char>(d);
+        int     i = static_cast<int>(d);
+        float   f = static_cast<float>(d);
+
+        //CHAR Kontrolü
+        if (d < 0 || d > 127 || std::isnan(d) || std::isinf(d))
+            std::cout << "char: impossible" << std::endl;
+        else
+        {
+            if(std::isprint(c))
+                std::cout << "char: '" << c << "'" << std::endl;
+            else
+                std::cout << "char: Non displayable" << std::endl;
+        }
+
+        // INT Kontrolü
+        if (d > std::numeric_limits<int>::max() || d < std::numeric_limits<int>::min() || std::isnan(d) || std::isinf(d))
+          std::cout << "int: impossible" << std::endl;
+        else
+            std::cout << "int: " << i << std::endl;
+        
+        std::cout << std::fixed << std::setprecision(1);
+        std::cout << "float: " << f << "f" << std::endl;
+        std::cout << "double: " << d << std::endl;
+    }
 }
