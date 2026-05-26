25fd6b9f131fae0ca64589a1f6f2aef775dc5af2
Abdullah Taha Ustunsoy <abdullahustunsoy1@gmail.com>
2026-05-26 14:20:59 +0300

    ex02ye basladim arg kontrolü yapildi

---
+++ ex02/main.cpp
@@ -0,0 +1,75 @@
+#include "PmergeMe.hpp"
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <climits>
+
+int argControl(int argc, char **argv)
+{
+    for(int i = 1; i < argc; i++) //05 , 007 gibi sayılar kontrole eklenmeli mi ? bence gerek yok
+    {
+        std::stringstream ss(argv[i]);
+        std::string token;
+
+        while(ss >> token)
+        {
+            if(token[0] == '-')
+            {
+                std::cerr << "Error" << std::endl;
+                return (1);
+            }
+            if(token[0] == '0' || (token[0] != '+' && !isdigit(token[0])))
+            {
+                std::cerr << "Error" << std::endl;
+                return (1);
+            }
+            if(token[0] == '+')
+            {
+                if (token.size() == 1) 
+                {
+                    std::cerr << "Error" << std::endl;
+                    return (1);
+                }
+                for(size_t j = 1; j < token.size(); j++)
+                {
+                    if(!isdigit(token[j]))
+                    {
+                        std::cerr << "Error" << std::endl;
+                        return (1);
+                    }
+                }
+            }
+            else
+            {
+                for(size_t j = 0; j < token.size(); j++)
+                {
+                    if(!isdigit(token[j]))
+                    {
+                        std::cerr << "Error" << std::endl;
+                        return (1);
+                    }
+                }
+            }
+            long longValue = std::strtol(token.c_str(), NULL, 10);
+            if (longValue > INT_MAX)
+            {
+                std::cerr << "Error" << std::endl;
+                return (1);
+            }
+        }
+    }
+    return (0);
+}
+
+int main(int argc, char **argv)
+{
+    if(argc != 2)
+    {
+        std::cerr << "Error" << std::endl;
+        return (1);
+    }
+    if(argControl(argc, argv))
+        return (1);
+    
+    return (0);
+}
